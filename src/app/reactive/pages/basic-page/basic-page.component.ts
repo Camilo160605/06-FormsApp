4629854fd6aa750e600a10357d974f63129f7e84
Camilo160605 <camilo1665ober@gmail.com>
2023-12-04 08:38:44 -0500

    forms has been created

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  // public myForm : FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage : new FormControl(0),
+  // });
+
+  public myForm : FormGroup = this.fb.group({
+    
+  })
+
+  constructor(private fb : FormBuilder){}
+
+
 
 }
---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+interface menuItem {
+  title : string;
+  route : string ;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: [
+  ]
+})
+export class SideMenuComponent {
+
+  public reactiveMenu : menuItem[] = [
+    {title: 'Basicos', route:'./reactive/basic'},
+    {title: 'Dínamicos', route:'./reactive/dynamic'},
+    {title: 'Switches', route:'./reactive/switches'},
+  ]
+  public authMenu : menuItem[] = [
+    {title: 'Registro', route:'./auth'}, 
+  ]
+
+}
