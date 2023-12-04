7f07e7ae90b04ee122d1ad31d0e2bee5aa1e07b9
Camilo160605 <camilo1665ober@gmail.com>
2023-12-04 15:12:23 -0500

    Some things doesnt works correctly

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // public myForm : FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -15,16 +15,41 @@ export class BasicPageComponent {
   // });
 
   public myForm : FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [ Validators.required, Validators.minLength( 3 ) ] ],
+    price: [0, [ Validators.required, Validators.min( 0 ) ] ],
+    inStorage: [0, [ Validators.required, Validators.min( 0 ) ] ],
   })
 
   constructor(private fb : FormBuilder){}
 
+  ngOnInit(): void {
+  }
+
+  isValidField(field : string ) : boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field : string ) : string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for ( const key of Object.keys(errors) ) {
+        console.log(key)
+    }
+    return 'hola mundo';
+  }
+
   onSave(): void {
 
-    if( this.myForm.invalid ) return
+    if( this.myForm.invalid ) {
+      
+      this.myForm.markAllAsTouched();
+
+      return
+    }
 
     console.log(this.myForm.value);
     
