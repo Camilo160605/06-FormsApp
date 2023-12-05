d5d58716c79fd0db0ca31959f1fd6dbe63a3d43a
Camilo160605 <camilo1665ober@gmail.com>
2023-12-05 18:15:52 -0500

    swithces page has been created for me

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -10,20 +10,44 @@ export class SwitchesPageComponent {
 
 
   public myForm : FormGroup = this.fb.group({
-    gender: ['m', Validators.required],
+    gender: ['M', Validators.required],
     wantNotifications: [true , Validators.required],
     termAndConditions: [false, Validators.requiredTrue ],
    });
 
   constructor( private fb : FormBuilder ){}
 
-  
+  isValidField(field : string ) : boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field : string ) : string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for ( const key of Object.keys(errors) ) {
+        switch( key ){
+          case 'required' :
+            return 'Este campo es requerido'
+          case 'minlength' :
+            return `Mínimo ${ errors['minlength'].requiredLength} caracteres`
+        }
+        
+    }
+    return null;
+  }
+
   onSave(){
 
     if ( this.myForm.invalid ) {
       this.myForm.markAllAsTouched();
       return 
     }
+    console.log(this.myForm.value);
+    
 
   }
 
