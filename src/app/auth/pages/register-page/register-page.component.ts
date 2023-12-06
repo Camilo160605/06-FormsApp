62e8a5120124c72946408fdbb5fe2024ae026996
Camilo160605 <camilo1665ober@gmail.com>
2023-12-06 14:58:13 -0500

    ValidatorService has been added

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validator.service';
 import * as CustomValidators from 'src/app/shared/validators/validators';
 
 @Component({
@@ -8,17 +9,21 @@ import * as CustomValidators from 'src/app/shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm : FormGroup = this.fb.group({
-    name : ['',[Validators.required, Validators.pattern(CustomValidators.firstNameAndLastnamePattern)]],
-    email : ['',[Validators.required, Validators.pattern(CustomValidators.emailPattern)]],
+    name : ['',[Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+    email : ['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
     username : ['',[Validators.required, CustomValidators.cantBeStrider]],
     password : ['',[Validators.required, Validators.minLength(6)]],
     password2 : ['',[Validators.required]],
   })
 
-  constructor( private fb : FormBuilder ){}
+  constructor( 
+    private fb : FormBuilder,
+    private validatorService : ValidatorsService
+    ){}
 
   isValidField( field : string ){
     // TODO: realizar validaciones
+    return this.validatorService.isValidField( this.myForm, field )
   }
 
   onSubmit() : void {
