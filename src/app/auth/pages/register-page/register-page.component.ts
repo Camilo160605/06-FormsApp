35b4e5f753084c675d5f1b540cbd91a770aaf2ca
Camilo160605 <camilo1665ober@gmail.com>
2023-12-06 14:31:44 -0500

    validators has veen adjusted

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as CustomValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -7,9 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterPageComponent {
 
   public myForm : FormGroup = this.fb.group({
-    name : ['',[Validators.required]],
-    email : ['',[Validators.required]],
-    username : ['',[Validators.required]],
+    name : ['',[Validators.required, Validators.pattern(CustomValidators.firstNameAndLastnamePattern)]],
+    email : ['',[Validators.required, Validators.pattern(CustomValidators.emailPattern)]],
+    username : ['',[Validators.required, CustomValidators.cantBeStrider]],
     password : ['',[Validators.required, Validators.minLength(6)]],
     password2 : ['',[Validators.required]],
   })
